acb70d32c16c4355fd039a9e1e404b111b94bcee
Dennis van den Berg <dennisvandenberg13100@gmail.com>
2022-10-05 18:06:21 -0300

    auth0 implemented

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,22 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import LoginButton from './LoginButton'
+import LogoutButton from './LogoutButton'
+import Profile from './Profile'
 
 function Home() {
     return (
         <div>
             <h1>This is the home page</h1>
             <div>
-            <Link to="/about">Click here to view our about page</Link>
+                <Link to="/about">Click here to view our about page</Link>
             </div>
             <div>
-            <Link to="/contact">Click here to view our contact page</Link>
+                <Link to="/contact">Click here to view our contact page</Link>
             </div>
+            <LoginButton />
+            <LogoutButton />
+            <Profile />
         </div>
     )
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom'
+import { Auth0Provider } from '@auth0/auth0-react'
 
-ReactDOM.render(
+const container = document.getElementById('root');
+const root = createRoot(container)
+root.render(
   <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  document.getElementById('root')
+   <Auth0Provider
+      domain="dev-kb35fzti.us.auth0.com"
+      clientId="azs6YETpLqiwDdhg6mun8Levnoj3IB3r"
+      redirectUri={window.location.origin}
+    >
+      <App />
+    </Auth0Provider>
+  </BrowserRouter>
 );
 
